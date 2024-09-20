d5790aea53cccbc6dc9c3f7f6ace9cd39ea69690
Abroradumalikov <abrorabdumalikov1072@gmail.com>
2024-09-20 13:53:05 +0500

    Update mode.js
    Signed-off-by: Abroradumalikov <abrorabdumalikov1072@gmail.com>

--- js/mode.js
+++ js/mode.js
@@ -33,7 +33,6 @@ const checking =
     ? "O'zbekistoning aholisi o'rtachadat yuqori"
     : "O'zbekistoning aholisi o'rta darjadan past";
 console.log(checking);
-header__button-menu
 const hamburger = document.querySelector(".hamburger");
 const button = document.querySelector(".header__button-menu");
 
@@ -41,4 +40,5 @@ hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("hamburger-1");
   button.classList.toggle("acc");
 });
+
 
